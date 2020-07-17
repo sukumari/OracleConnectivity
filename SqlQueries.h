b9bad243393c33c4205494d6e75ceaa18ccfcc65
sukumari <68416474+sukumari@users.noreply.github.com>
2020-07-17 04:36:11 -0700

    C/C++ program to connect to Oracle using ODBC and OCI drive - POC

---
+++ SqlQueries.h
@@ -0,0 +1,113 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> totalQueriesToRun();
+
+vector<string> sql_queries;
+
+vector<string> totalQueriesToRun() {
+string sql = "SELECT view_name FROM dba_views WHERE OWNER='SYS'";
+
+string sql1 = "SELECT nvl(VIEW_TYPE, 'STANDARD') as type, " \
+              "CASE " \
+                "WHEN VIEW_TYPE IS NULL THEN '' " \
+                "WHEN VIEW_TYPE = 'XMLTYPE' THEN ' OF ' || TYPE_TEXT || ' WITH OBJECT IDENTIFIER ' || " \
+                  "CASE " \
+                    "WHEN OID_TEXT = null OR OID_TEXT = 'DEFAULT' THEN 'DEFAULT' " \
+                    "ELSE '(' || OID_TEXT || ')' " \
+                  "END " \
+                "ELSE ' OF ' || VIEW_TYPE_OWNER || '.' || VIEW_TYPE || " \
+                  "CASE " \
+                    "WHEN SUPERVIEW_NAME IS NULL THEN ' WITH OBJECT IDENTIFIER ' || " \
+                      "CASE " \
+                        "WHEN OID_TEXT = null OR OID_TEXT = 'DEFAULT' THEN 'DEFAULT' " \
+                        "ELSE '(' || OID_TEXT || ')' " \
+                      "END " \
+                    "ELSE ' UNDER ' || SUPERVIEW_NAME " \
+                  "END " \
+              "END " \
+              "FROM dba_views " \
+              "WHERE owner='SYS' AND view_name='DBMS_SQLPATCH_STATE' ";
+
+string sql2 = "SELECT column_name " \
+              "FROM dba_tab_cols " \
+              "WHERE owner='SYS' AND table_name='DBMS_SQLPATCH_STATE' " \
+              "ORDER BY column_id ";
+              
+string sql3 = "SELECT " \
+                "DECODE(dc.generated, 'GENERATED NAME', '', 'CONSTRAINT ' || dc.constraint_name || ' ') constname, " \
+                "CASE dc.CONSTRAINT_TYPE " \
+                  "WHEN 'P' THEN 'PRIMARY KEY ' " \
+                  "WHEN 'U' THEN 'UNIQUE ' " \
+                  "WHEN 'R' THEN 'FOREIGN KEY ' " \
+                "END, " \
+                "listagg(dcc.column_name, ', ')  WITHIN GROUP (ORDER BY dcc.position), " \
+                "DECODE(dc.deferrable, 'DEFERRABLE', ' DEFERRABLE INITIALLY ' || dc.deferred, ''), " \
+                "CASE " \
+                  "WHEN dc.r_owner IS NULL THEN '' " \
+                  "ELSE ' REFERENCES ' || dc.r_owner || '.' || dccR.table_name " \
+                          "|| ' (' || listagg(dccR.column_name, ', ') WITHIN GROUP (ORDER BY dccR.column_name) || ')' " \
+                "END, " \
+                "CASE " \
+                  "WHEN delete_rule IS NULL THEN '' " \
+                  "WHEN delete_rule = 'CASCADE' THEN ' ON DELETE CASCADE' " \
+                  "ELSE ' ON DELETE SET NULL' " \
+                "END, " \
+                "CASE dc.status " \
+                  "WHEN 'ENABLED' THEN " \
+                    "DECODE(dc.validated, 'VALIDATED', '', ' ENABLE NOVALIDATE') " \
+                  "ELSE " \
+                    "DECODE(dc.validated, 'VALIDATED', ' DISABLE VALIDATE', ' DISABLE') " \
+                "END, " \
+                "DECODE(dc.rely, 'RELY', ' RELY', '') " \
+              "FROM dba_constraints dc " \
+              "LEFT OUTER JOIN dba_cons_columns dcc ON dcc.owner=dc.owner AND dcc.CONSTRAINT_NAME = dc.constraint_name " \
+              "LEFT OUTER JOIN dba_cons_columns dccR ON dc.r_owner=dccR.OWNER AND dc.R_CONSTRAINT_NAME = dccR.constraint_name " \
+              "WHERE (dc.constraint_type = 'P' OR dc.constraint_type = 'U' OR dc.constraint_type = 'R') " \
+                "AND dc.owner='SYS' and dc.table_name='DBMS_SQLPATCH_STATE' " \
+                "AND dcc.column_name!='SYS_NC_ROWINFO$' AND dcc.column_name!='SYS_NC_OID$' " \
+              "GROUP BY " \
+                "dc.constraint_type, dc.constraint_name, dc.generated, dc.deferrable, dc.deferred, " \
+                "dc.status, dc.validated, dc.rely, dc.r_owner, dccR.table_name, dc.delete_rule " \
+              "ORDER BY dc.CONSTRAINT_TYPE, dc.CONSTRAINT_NAME ";
+
+string sql4 = "SELECT " \
+                "CASE " \
+                  "WHEN " \
+                    "VIEW_TYPE IS NULL " \
+                      "AND " \
+                    "(SELECT COUNT(column_name) FROM dba_tab_cols WHERE owner='SYS' AND table_name='DBMS_SQLPATCH_STATE') != 0 " \
+                  "THEN chr(10) || chr(9) || ')' " \
+                  "ELSE '' " \
+                "END, " \
+                "nvl(text, ''), " \
+                "CASE " \
+                  "WHEN text IS NULL THEN text_vc " \
+                  "ELSE '' " \
+                "END " \
+              "FROM dba_views " \
+              "WHERE owner='SYS' AND view_name='DBMS_SQLPATCH_STATE' ";
+
+string sql5 = "SELECT grantee, privilege, grantor, " \
+                "CASE " \
+                  "WHEN GRANTABLE = 'Y' OR GRANTABLE = 'YES' THEN ' WITH GRANT OPTION' " \
+                  "ELSE '' " \
+                "END, " \
+                "CASE " \
+                  "WHEN HIERARCHY = 'Y' OR HIERARCHY = 'YES' THEN ' WITH HIERARCHY OPTION' " \
+                  "ELSE '' " \
+                "END " \
+              "FROM dba_tab_privs " \
+              "WHERE owner='SYS' AND table_name='DBMS_SQLPATCH_STATE' " \
+              "ORDER BY grantee, privilege, grantor ";
+    sql_queries.push_back(sql);
+    sql_queries.push_back(sql1);
+    sql_queries.push_back(sql2);
+    sql_queries.push_back(sql3);
+    sql_queries.push_back(sql4);
+    sql_queries.push_back(sql5);
+
+    return sql_queries;
+}
---
+++ TestOracleConnectionWithOCI.cpp
@@ -0,0 +1,87 @@
+// oracle_test.cpp : Defines the entry point for the console application.
+//
+/*
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	return 0;
+}
+*/
+#include <iostream>  // for printf
+#include <stdlib.h>
+#include <ctime>
+#include <SQLAPI.h>
+#include "SqlQueries.h"
+
+using namespace std;
+
+clock_t START_TIMER;
+
+clock_t startTimer()
+{
+    return START_TIMER = clock();
+}
+
+void stopTimer(clock_t start = START_TIMER)
+{
+    cout
+        << "Elapsed time: "
+        << (clock() - start) / (double)CLOCKS_PER_SEC << "s"
+        << endl;
+}
+
+void createOracleConnection(SAConnection& con)
+{
+    string url ="(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.248.139.221)(PORT=1521))(CONNECT_DATA=(SERVICE_NAME=orcl.tripwirelab.com)))";
+    con.setOption("UseApi") = "OCI8";
+    con.Connect(url.c_str(), "scott", "tiger", SA_Oracle_Client);
+    if(con.isConnected()) {
+        cout << "Database connected sucessfully" << "\n";
+    }   
+}
+
+int main()
+{
+    SAConnection con; // connection object
+
+    vector<string> view_sql_queries = totalQueriesToRun();
+    // Declaring iterator to a vector 
+    vector<string>::iterator itr; 
+    try {
+        // connect to database (Oracle in our example)
+        createOracleConnection(con);
+        for (int i =1; i <=5; ++i) {
+            SACommand cmd(&con);
+            startTimer();
+            for (int i =1; i <=50; ++i) {
+                for (itr = view_sql_queries.begin(); itr < view_sql_queries.end(); itr++) {
+                    cmd.setCommandText((*itr).c_str());
+                    cmd.Execute();
+                }
+            }
+            stopTimer();
+        }
+
+        // commit changes on success
+        con.Commit();
+
+        if(con.isConnected()) {
+            con.Disconnect();
+        }
+        cout << "Press any key to exit" << "\n";
+        std::cin.get();
+    }
+    catch(SAException &x)
+    {
+       cout << "Exception caught ......."<<"\n";
+        cout <<  (const char*)x.ErrText();
+        cout << "Press any key to exit" << "\n";
+        std::cin.get();
+    }
+    
+    return 0;
+}
+
